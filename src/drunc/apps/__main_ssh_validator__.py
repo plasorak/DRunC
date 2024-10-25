f8f5692802eeb62e55f7b13bb340342e68037e69
PawelPlesniak <pmp115@ic.ac.uk>
2024-10-25 14:57:44 +0200

    Changing filename to configuration

--- src/drunc/apps/__main_ssh_validator__.py
+++ src/drunc/apps/__main_ssh_validator__.py
@@ -11,8 +11,8 @@ from drunc.utils.utils import expand_path
 from drunc.process_manager.ssh_process_manager import on_parent_exit
 from drunc.process_manager.oks_parser import collect_apps
 
-def validate_ssh_connection(confiuguration_filename:str, session_name:str):
-    conf = find_configuration(confiuguration_filename)
+def validate_ssh_connection(configuration:str, session_name:str):
+    conf = find_configuration(configuration)
     db = conffwk.Configuration(f"oksconflibs:{conf}")
     session_dal = db.get_dal(class_name="Session", uid=session_name)
     disabled_applications = [app.id for app in session_dal.disabled]
@@ -39,11 +39,11 @@ def validate_ssh_connection(confiuguration_filename:str, session_name:str):
             rprint(f"Failed to SSH onto host [red]{user_host}[/red]. Exception raised: {str(e)}")
 
 @click.command()
-@click.argument('filename', type=str, nargs=1)
+@click.argument('configuration', type=str, nargs=1)
 @click.argument('session', type=str, nargs=1)
 def main(filename:str, session:str):
     """
-    The script validates the ability to SSH onto all of the hosts required by the configuration <filename> session <session> applications.
+    The script validates the ability to SSH onto all of the hosts required by the configuration <configuration> session <session> applications.
     """
     validate_ssh_connection(filename, session)
 
