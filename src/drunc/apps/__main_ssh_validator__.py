3b7eac6895ef6fec6a0eb43700c4b20c5900ac55
PawelPlesniak <pmp115@ic.ac.uk>
2024-10-25 12:26:15 +0200

    Removing redundant arg

--- src/drunc/apps/__main_ssh_validator__.py
+++ src/drunc/apps/__main_ssh_validator__.py
@@ -11,7 +11,7 @@ from drunc.utils.utils import expand_path
 from drunc.process_manager.ssh_process_manager import on_parent_exit
 from drunc.process_manager.oks_parser import collect_apps
 
-def validate_ssh_connection(confiuguration_filename:str, session_name:str, verbose:bool):
+def validate_ssh_connection(confiuguration_filename:str, session_name:str):
     conf = find_configuration(confiuguration_filename)
     db = conffwk.Configuration(f"oksconflibs:{conf}")
     session_dal = db.get_dal(class_name="Session", uid=session_name)
@@ -39,12 +39,11 @@ def validate_ssh_connection(confiuguration_filename:str, session_name:str, verbo
             rprint(f"Failed to SSH onto host [red]{user_host}[/red]. Exception raised: {str(e)}")
 
 @click.command()
-@click.option('-v', '--verbose', is_flag=True, type=bool, default=False, help='Verbose output')
 @click.argument('filename', type=str, nargs=1)
 @click.argument('session', type=str, nargs=1)
-def main(filename:str, session:str, verbose:bool):
+def main(filename:str, session:str):
     """
     The script validates the ability to SSH onto all of the hosts required by the configuration <filename> session <session> applications.
     """
-    validate_ssh_connection(filename, session, verbose)
+    validate_ssh_connection(filename, session)
 
