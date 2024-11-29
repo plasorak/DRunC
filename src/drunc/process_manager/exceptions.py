e4d47049f1809af24681e4b1cf6c14be0d8fa607
Pierre Lasorak <pierre.lasorak@gmail.com>
2024-11-29 19:21:02 +0100

    move all the k8s into process_manager.excpetions

--- src/drunc/process_manager/exceptions.py
+++ src/drunc/process_manager/exceptions.py
@@ -1,5 +1,13 @@
-from drunc.exceptions import DruncSetupException
+from drunc.exceptions import DruncSetupException, DruncCommandException, DruncException
 
 class UnknownProcessManagerType(DruncSetupException):
     def __init__(self, pm_type):
         super().__init__(f'\'{pm_type}\' is not handled/unknown')
+
+class BadQuery(DruncCommandException):
+    def __init__(self, txt):
+        super(BadQuery, self).__init__(txt, code_pb2.INVALID_ARGUMENT)
+
+class DruncK8sNamespaceAlreadyExists(DruncException): # Exceptions that gets thrown when namespaces already exists
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
