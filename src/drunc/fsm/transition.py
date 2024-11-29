01938be6127c61f09ff4e09c06cabf40179c229e
Pierre Lasorak <pierre.lasorak@gmail.com>
2024-11-29 13:16:01 +0100

    add TransitoinSequences

--- src/drunc/fsm/transition.py
+++ src/drunc/fsm/transition.py
@@ -1,12 +1,13 @@
 from druncschema.controller_pb2 import Argument
 
 class Transition:
-    def __init__(self, name, source, destination, arguments=[], help:str=''):
+    def __init__(self, name:str, source:str, destination:str, arguments:[Argument]=[], help:str=''):
         self.source = source
         self.destination = destination
         self.name = name
         self.arguments = arguments
         self.help = help
+
 
     def __eq__(self, another):
         same_name = hasattr(another, 'name') and self.name == another.name
@@ -19,3 +20,19 @@ class Transition:
 
     def __str__(self):
         return f'\"{self.name}\": \"{self.source}\" → \"{self.destination}\"'
+
+    def get_arguments(self):
+        return self.arguments
+
+class TransitionSequence(Transition):
+    def __init__(self, name:str, transitions:[Transition]=[]):
+        self.sequence = transitions
+        help_str = 'A sequence of transtions:\n'+'\n'.join([f' - {t.name}: {t.help}' for t in transitions])
+        dest = transitions[-1].destination
+        source = '|'.join([t.source for t in transitions])
+        arguments = []
+        for t in transitions:
+            arguments += t.get_arguments()
+
+        super().__init__(name, source, dest, arguments, help=help_str)
+
