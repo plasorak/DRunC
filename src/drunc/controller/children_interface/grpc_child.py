8b3521177f0da11095236fb220d2f7e3071d6815
Pierre Lasorak <pierre.lasorak@gmail.com>
2024-12-02 19:59:28 +0100

    nuke confhandler for gRPC children, moveimport at the top

--- src/drunc/controller/children_interface/grpc_child.py
+++ src/drunc/controller/children_interface/grpc_child.py
@@ -1,52 +1,43 @@
+import grpc
+from logging import getLogger
+from time import sleep
+
+from druncschema.controller_pb2_grpc import ControllerStub
+from druncschema.request_response_pb2 import Description, Response
+
+from drunc.broadcast.client.broadcast_handler import BroadcastHandler
 from drunc.controller.children_interface.child_node import ChildNode
-from drunc.utils.utils import ControlType
+from drunc.controller.children_interface.types import ControlType
 from drunc.controller.utils import send_command
-from drunc.utils.configuration import ConfHandler
-import grpc as grpc
 from drunc.exceptions import DruncSetupException
-from druncschema.request_response_pb2 import Response
-
-
-class gRCPChildConfHandler(ConfHandler):
-    def get_uri(self):
-        for service in self.data.controller.exposes_service:
-            if self.data.controller.id+"_control" in service.id:
-                return f"{service.protocol}://{self.data.controller.runs_on.runs_on.id}:{service.port}"
-        raise DruncSetupException(f"gRPC API child node {self.data.controller.id} does not expose a control service")
+from drunc.utils.configuration import ConfTypes
+from drunc.utils.grpc_utils import ServerUnreachable
 
 
 class gRPCChildNode(ChildNode):
-    def __init__(self, name, configuration:gRCPChildConfHandler, init_token, uri):
+    def __init__(self, name, configuration, init_token, uri):
         super().__init__(
             name = name,
-            node_type = ControlType.gRPC, 
+            node_type = ControlType.gRPC,
             configuration  = configuration
         )
 
-        from logging import getLogger
         self.log = getLogger(f'{self.name}-grpc-child')
-
 
         host, port = uri.split(":")
         port = int(port)
 
         if port == 0:
-            from drunc.exceptions import DruncSetupException
-            raise DruncSetupException(f"Application {name} does not expose a control service in the configuration, or has not advertised itself to the application registry service, or the application registry service is not reachable.")
+            raise DruncSetupException(f"Application \'{name}\' does not expose a control service in the configuration, or has not advertised itself to the connectivity service, or the connectivity service is not reachable.")
 
         self.uri = f"{host}:{port}"
-
-        from druncschema.controller_pb2_grpc import ControllerStub
-        import grpc
 
         self.channel = grpc.insecure_channel(self.uri)
         self.controller = ControllerStub(self.channel)
 
-        from druncschema.request_response_pb2 import Description
         desc = Description()
         ntries = 20
-        from drunc.utils.grpc_utils import ServerUnreachable
-        from drunc.exceptions import DruncSetupException
+
 
         for itry in range(ntries):
             try:
@@ -62,7 +53,6 @@ class gRPCChildNode(ChildNode):
                     raise e
                 else:
                     self.log.info(f'Could not connect to the controller ({self.uri}), trial {itry+1} of {ntries}')
-                    from time import sleep
                     sleep(5)
 
             except ServerUnreachable as e:
@@ -70,7 +60,7 @@ class gRPCChildNode(ChildNode):
             else:
                 self.log.info(f'Connected to the controller ({self.uri})!')
                 break
-        self.start_listening(desc.broadcast)
+        # self.start_listening(desc.broadcast)
 
     def __str__(self):
         return f'\'{self.name}@{self.uri}\' (type {self.node_type})'
@@ -80,15 +70,8 @@ class gRPCChildNode(ChildNode):
 
 
     def start_listening(self, bdesc):
-        from drunc.broadcast.client.configuration import BroadcastClientConfHandler
-        from drunc.broadcast.client.broadcast_handler import BroadcastHandler
-        from drunc.utils.configuration import ConfTypes
-        self.broadcast = BroadcastHandler(
-            BroadcastClientConfHandler(
-                data = bdesc,
-                type = ConfTypes.ProtobufAny,
-            )
-        )
+        pass
+        # self.broadcast = BroadcastHandler()
 
     def get_status(self, token) -> Response:
         return send_command(
@@ -107,7 +90,7 @@ class gRPCChildNode(ChildNode):
 
         self.controller = None
         self.channel = None
-        self.broadcast.stop()
+        # self.broadcast.stop()
         pass
 
     def propagate_command(self, command, data, token) -> Response:
@@ -118,5 +101,3 @@ class gRPCChildNode(ChildNode):
             rethrow = True,
             data = data
         )
-
-
