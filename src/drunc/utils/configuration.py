4c305de90ae189947ff6e162621e6ea13ccd5109
Pierre Lasorak <pierre.lasorak@gmail.com>
2024-12-03 15:38:56 +0100

    rename the logger

--- src/drunc/utils/configuration.py
+++ src/drunc/utils/configuration.py
@@ -86,7 +86,7 @@ class ConfHandler:
     def __init__(self, data=None, type=ConfTypes.PyObject, oks_key:OKSKey=None, *args, **kwargs):
         from logging import getLogger
         self.class_name = self.__class__.__name__
-        self.log = getLogger(self.class_name)
+        self.log = getLogger(f'drunc.{self.class_name}')
         self.initial_type = type
         self.initial_data = data
         self.root_id = 0
