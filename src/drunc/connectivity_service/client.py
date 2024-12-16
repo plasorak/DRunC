68ed51223a2a0cef0953cc8d55b5317271082078
Kurt Biery <biery@daq.fnal.gov>
2024-12-16 09:14:16 -0600

    Added missing exception type in 'except' statement in the publish function.

--- src/drunc/connectivity_service/client.py
+++ src/drunc/connectivity_service/client.py
@@ -130,7 +130,7 @@ class ConnectivityServiceClient:
                     ignore_errors = True
                 ).raise_for_status()
                 break
-            except (HTTPError, ConnectionError) as e:
+            except (HTTPError, ConnectionError, ReadTimeout) as e:
                 from time import sleep
                 sleep(0.2)
                 continue
