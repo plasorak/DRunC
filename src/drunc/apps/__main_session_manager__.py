dbbbbbfe2659b63e062069168ccad0b42e77f2c0
James Paul Turner <james.turner@imperial.ac.uk>
2025-02-12 16:54:54 +0000

    Stub sess man bin.

---
+++ src/drunc/apps/__main_session_manager__.py
@@ -0,0 +1,13 @@
+# from drunc.session_manager.interface.session_manager import session_manager_cli
+from drunc.utils.utils import print_traceback
+
+def main():
+    try:
+        # TODO: session_manager_cli()
+        pass
+    except Exception:
+        print_traceback()
+
+
+if __name__ == '__main__':
+    main()
