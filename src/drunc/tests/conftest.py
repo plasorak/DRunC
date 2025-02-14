e446e771db6e9aada974217265a6b76956cc0abe
Pierre Lasorak <pierre.lasorak@gmail.com>
2025-02-14 15:47:12 +0100

    correct path

--- src/drunc/tests/conftest.py
+++ src/drunc/tests/conftest.py
@@ -8,7 +8,7 @@ def load_test_config():
     if DUNEDAQ_DB_PATH is None:
         DUNEDAQ_DB_PATH = ''
     cwd = Path(os.path.abspath(__file__))
-    test_configs = cwd.parent / ".." / ".." / ".." / ".." / "config" / "tests"
+    test_configs = cwd.parent / ".." / ".." / ".." / "config" / "tests"
     test_configs = test_configs.resolve()
     DUNEDAQ_DB_PATH += f":{str(test_configs)}"
     os.environ["DUNEDAQ_DB_PATH"] = DUNEDAQ_DB_PATH
